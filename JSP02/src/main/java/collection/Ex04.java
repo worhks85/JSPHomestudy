8691e0189fe83cea6499d0eab01665e8acf65d22
JaeKwan Kim <worhks5017@naver.com>
2022-04-27 23:36:34 +0900

    4/27 Practice

---
+++ JSP02/src/main/java/collection/Ex04.java
@@ -0,0 +1,20 @@
+package collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Ex04 {
+
+	public static void main(String[] args) {
+		Map<String,Integer> map = new HashMap<>();
+		// 데이터 넣기
+		map.put("홍길동",0);
+		map.put("성춘향",1);
+		
+		
+		
+		
+		
+	}
+
+}
